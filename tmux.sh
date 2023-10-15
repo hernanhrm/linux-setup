57202e9fe76cda7133518d4721bae74df85897f5
Hernan Reyes <hernan_rm@outlook.es>
2023-10-14 18:26:39 -0600

    Update tmux.sh

--- tmux.sh
+++ tmux.sh
@@ -40,3 +40,24 @@ run '~/.tmux/plugins/tpm/tpm'
 
 touch ~/.tmux.conf
 echo "$config" > ~/.tmux.conf
+
+sudo touch /usr/local/tmux-sessionizer
+
+# Contenido del archivo
+sessionizer="#!/usr/bin/env bash
+
+session=\$(echo -e \"\$(find ~/dotfiles -mindepth 0 -maxdepth 0 -type d)\\n\$(find ~/dotfiles/config/.config/nvim -mindepth 0 -maxdepth 0 -type d)\\n\$(find ~/code -mindepth 2 -maxdepth 2 -type d)\" | fzf)
+
+session_name=\$(basename \"\$session\" | tr . _)
+
+if ! tmux has-session -t \"\$session_name\" 2> /dev/null; then
+  tmux new-session -s \"\$session_name\" -c \"\$session\" -d
+fi
+
+tmux switch-client -t \"\$session_name\"
+"
+
+# Guardar el contenido en un archivo
+echo "$sessionizer" > tmux-sessionizer
+
+sudo chmod +x /usr/local/tmux-sessionizer
