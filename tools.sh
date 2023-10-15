dc9a0d00da0f28c38de55fb8ad24dc6155dc49e9
Hernan Reyes <hernan_rm@outlook.es>
2023-10-14 18:14:52 -0600

    Create tools.sh

---
+++ tools.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# install tweaks
+sudo add-apt-repository universe
+sudo apt -y install gnome-tweak-tool
+
+## install chrome extension for gnome
+sudo apt -y install chrome-gnome-shell
+
+# Install homebrew's dependencies
+sudo apt -y install build-essential procps curl file
+
+# Install deepin system monitor GUI
+sudo add-apt-repository ppa:ubuntudde-dev/stable
+sudo apt update
+sudo apt -y install deepin-system-monitor
+
+# install screenshot app
+sudo apt update
+sudo apt -y install flameshot
+
+# Install Main menu
+sudo apt update
+sudo apt -y install alacarte
+
+# Install vim
+sudo apt -y install vim
+
+# Install fuse because it's required by jetbrains toolbox
+sudo add-apt-repository universe
+sudo apt -y install libfuse2
