4e3478c4906fceff200a1bd07f197aa177ee7fbc
Hernan Reyes <hernan_rm@outlook.es>
2023-10-14 18:47:20 -0600

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -2,3 +2,6 @@
 
 sudo apt -y install zsh
 sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+
+echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.zshrc
+source ~/.zshrc
