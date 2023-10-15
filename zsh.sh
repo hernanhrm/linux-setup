fb92307887f39b910708d556b5e9560753ac3992
Hernan Reyes <hernan_rm@outlook.es>
2023-10-14 19:10:53 -0600

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -8,4 +8,11 @@ echo "export GOROOT=/home/linuxbrew/.linuxbrew/Cellar/go/1.21.3" >> ~/.zshrc
 echo "export GOPATH=$HOME/go" >> ~/.zshrc
 echo "export GOBIN=$GOPATH/bin" >> ~/.zshrc
 
+# Contenido a agregar
+content="bindkey -s '^f' 'tmux-sessionizer\n'
+eval \"\$(starship init zsh)\""
+
+# Agrega el contenido al archivo .zshrc
+echo -e "$content" >> ~/.zshrc
+
 source ~/.zshrc
