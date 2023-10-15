0176b6dd52076883a1a97bdddef22359d1cbe0fe
Hernan Reyes <hernan_rm@outlook.es>
2023-10-14 18:06:13 -0600

    Create homebrew.sh

---
+++ homebrew.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt-get install build-essential procps curl file git
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
+test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.bashrc
+echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.zshrc
+source ~/.bashrc
+source ~/.zshrc
