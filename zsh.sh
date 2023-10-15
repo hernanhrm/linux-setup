00de815682f3833f8bca9d1db45420432e035680
Hernan Reyes <hernan_rm@outlook.es>
2023-10-14 19:10:31 -0600

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -6,6 +6,6 @@ sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O 
 echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.zshrc
 echo "export GOROOT=/home/linuxbrew/.linuxbrew/Cellar/go/1.21.3" >> ~/.zshrc
 echo "export GOPATH=$HOME/go" >> ~/.zshrc
-z
+echo "export GOBIN=$GOPATH/bin" >> ~/.zshrc
 
 source ~/.zshrc
