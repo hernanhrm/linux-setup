1e4f8624950cd024e00b17618de2ae03d477a014
Hernan Reyes <hernan_rm@outlook.es>
2023-10-14 19:09:19 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 mkdir ~/code/personal -p
+mkdir ~/go/pkg -p
+mkdir ~/go/src -p
+mkdir ~/go/bin -p
+
+sudo add-apt-repository ppa:agornostal/ulauncher && sudo apt update && sudo apt install ulauncher
 
 ./git.sh
 ./ssh.sh
