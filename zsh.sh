da868d87614c0f7ca2c8daa99d245c51b039dad1
Hernan Reyes <hernan_rm@outlook.es>
2023-10-14 19:07:42 -0600

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -6,7 +6,6 @@ sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O 
 echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.zshrc
 echo "export GOROOT=/home/linuxbrew/.linuxbrew/Cellar/go/1.21.3" >> ~/.zshrc
 echo "export GOPATH=$HOME/go" >> ~/.zshrc
-echo -e "bindkey -s '^f' 'tmux-sessionizer\n" >> ~/.zshrc
-echo -e "eval \"\$(starship init zsh)\"" >> ~/.zshrc
+z
 
 source ~/.zshrc
